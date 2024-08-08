48c0f17b8716aa0aa65c036cafca354af550f7dd
Sathysree2004 <sathyasree029@gmailcom>
2024-08-08 11:01:07 -0700

    basic

---
+++ src/main/java/core_java/sathya.java
@@ -0,0 +1,14 @@
+package core_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class sathya
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println( "sathya" );
+    }
+}
